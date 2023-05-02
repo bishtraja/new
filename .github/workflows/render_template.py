1d6f25d324f52e2aab88898951e7dc064e82272f
bishtraja <129159480+bishtraja@users.noreply.github.com>
2023-05-02 14:22:31 +0530

    Update render_template.py

--- .github/workflows/render_template.py
+++ .github/workflows/render_template.py
@@ -1,13 +1,18 @@
-import jinja2
-import sys
+import os
+from jinja2 import Template
 
-def main():
-    template_path = sys.argv[1]
-    with open(template_path, 'r') as f:
-        template = jinja2.Template(f.read())
-    rendered_template = template.render()
-    with open('final-email.html', 'w') as f:
-        f.write(rendered_template)
+with open("email_template.html", "r") as f:
+    template = Template(f.read())
 
-if __name__ == '__main__':
-    main()
+# Render the template with the desired variables
+rendered_template = template.render(
+    job_status=os.environ["JOB_STATUS"],
+    github_repository=os.environ["GITHUB_REPOSITORY"],
+    github_run_number=os.environ["GITHUB_RUN_NUMBER"],
+    github_ref=os.environ["GITHUB_REF"],
+    github_head_commit_message=os.environ["GITHUB_HEAD_COMMIT_MESSAGE"],
+)
+
+# Write the rendered template back to the file
+with open("email_template.html", "w") as f:
+    f.write(rendered_template)
